496db557699283ead38a2427678662df82b70a86
Bruno Alvim <balvim@hotmail.com>
2020-08-28 00:50:08 -0300

    Implementado as ações de banco de dados MySql.

--- routes/products.js
+++ routes/products.js
@@ -1,38 +1,100 @@
 const express = require('express')
 const router  = express.Router()
+const mysql   = require('../mysql').pool
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        message: 'Usando o método GET dentro da rota de produtos'
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM products',
+            (error, result, field) => {
+                if (error) { return res.status(500).send({error: error}) }
+    
+                res.status(200).send({response: result})
+            }
+        )
     })
 })
 
 router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    }
-
-    res.status(201).send({
-        message: 'Usando o método POST dentro da rota de produtos',
-        created_product: product
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'INSERT INTO products (name, price) VALUES (?, ?)',
+            [req.body.name, req.body.price],
+            (error, result, field) => {
+                conn.release()
+    
+                if (error) { return res.status(500).send({error: error}) }
+    
+                res.status(201).send({
+                    message: 'Produto inserido com sucesso.',
+                    productId: result.insertId
+                })
+            }
+        )
     })
 })
 
 router.get('/:product_id', (req, res, next) => {
     const id = req.params.product_id
 
-    if (id === 'especial') {
-        res.status(200).send({
-            message: 'Você descobriu o id Especial',
-            id: id
-        })    
-    } else {
-        res.status(200).send({
-            message: 'Usando o método GET dentro da rota de produtos para um produto expecífico',
-            id: id
-        })    
-    }
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            'SELECT * FROM products WHERE product_id = ?',
+            [id],
+            (error, result, field) => {
+                if (error) { return res.status(500).send({error: error}) }
+    
+                res.status(200).send({response: result})
+            }
+        )
+    })
+})
+
+router.patch('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `UPDATE products SET
+                name          = ?, 
+                price         = ?
+             WHERE product_id = ?`,
+            [
+                req.body.name, 
+                req.body.price, 
+                req.body.product_id
+            ],
+            (error, result, field) => {
+                conn.release()
+    
+                if (error) { return res.status(500).send({error: error}) }
+    
+                res.status(202).send({
+                    message: 'Produto alterado com sucesso.'
+                })
+            }
+        )
+    })
+})
+
+router.delete('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        conn.query(
+            `DELETE FROM products WHERE product_id = ?`, [req.body.product_id],
+            (error, result, field) => {
+                conn.release()
+    
+                if (error) { return res.status(500).send({error: error}) }
+    
+                res.status(202).send({
+                    message: 'Produto removido com sucesso.'
+                })
+            }
+        )
+    })
 })
 
 module.exports = router
