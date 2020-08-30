ecf1b21c6ec75a6985f84e79f6f05a152cbe06dc
Bruno Alvim <balvim@hotmail.com>
2020-08-30 18:00:44 -0300

    Adicionando o middleware de autenticação no pedido

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,12 @@
 const express          = require('express')
 const router           = express.Router()
 const OrdersController = require('../controllers/orders-controller')
+const login            = require('../middleware/login')
 
 router.get('/', OrdersController.getOrders)
 router.get('/:order_id', OrdersController.getOrder)
-router.post('/', OrdersController.postOrder)
-router.patch('/', OrdersController.patchOrder)
-router.delete('/:order_id', OrdersController.deleteOrder)
+router.post('/', login.required, OrdersController.postOrder)
+router.patch('/', login.required, OrdersController.patchOrder)
+router.delete('/:order_id', login.required, OrdersController.deleteOrder)
 
 module.exports = router
